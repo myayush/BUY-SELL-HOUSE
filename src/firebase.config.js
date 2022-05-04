8165dd2acaf48f501e64990912c1405333bdaa3f
Ayush Kumar <rajeshkumarayushkumar1@gmail.com>
2022-05-04 20:43:28 +0530

    first commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyAM0QjS-FCU9--grOig1wLKEAEDO-3dmPk",
-  authDomain: "auth-426d8.firebaseapp.com",
-  projectId: "auth-426d8",
-  storageBucket: "auth-426d8.appspot.com",
-  messagingSenderId: "826389300520",
-  appId: "1:826389300520:web:735e795dcb55ab612cfc1d",
-  measurementId: "G-FC9P2GR4TW"
+ 
+ /* authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId:
+  appId: 
+  measurementId: 
+  */
 };
 
   const app =initializeApp(firebaseConfig);
